3339bfb7bca02e7cf63d58d170147c31a2b06a1c
Christopher Leja <christopher.leja@gmail.com>
2020-02-07 20:26:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -70,6 +70,7 @@ def gross_per_studio(collection)
     i += 1
   end
 studio
+break
   
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from

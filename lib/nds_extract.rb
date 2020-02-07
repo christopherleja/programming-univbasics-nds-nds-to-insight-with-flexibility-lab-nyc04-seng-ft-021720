502171b92d2704a5ca81358b06463a79c988d8af
Christopher Leja <christopher.leja@gmail.com>
2020-02-07 20:33:37 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -54,14 +54,13 @@ def gross_per_studio(collection)
   i = 0
 
   result = {}
-  studio_profit = 0
+ 
   
   while i < collection.length do
     studio_name = collection[i][:studio]
     profit = collection[i][:worldwide_gross]
      
       if !result[studio_name]  
-      studio_profit += profit
       result[studio_name] += studio_profit
       
       else
